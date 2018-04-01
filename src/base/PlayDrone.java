f2411e36d2b747811eacf02cc74a833f76bebab9
Pavel Shvarchov <shvarpa@gmail.com>
2018-04-01 19:51:52 +0300

    Added

--- src/base/PlayDrone.java
+++ src/base/PlayDrone.java
@@ -1,5 +1,7 @@
 package base;
 
-public class PlayDrone {
-
+public class PlayDrone extends Drone {
+	public PlayDrone() {
+		super("Toy",0,10,"Civillian","a");
+	}
 }
--- src/base/SpyDrone.java
+++ src/base/SpyDrone.java
@@ -1,5 +1,7 @@
 package base;
 
-public class SpyDrone {
-
+public class SpyDrone extends Drone {
+	public SpyDrone() {
+		super("Classified",1,50,"Army","c");
+	}
 }
