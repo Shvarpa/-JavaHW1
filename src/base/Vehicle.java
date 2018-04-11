2390c1041d4b4b17d1fb8a1bd0b49c457d81fb18
Pavel Shvarchov <shvarpa@gmail.com>
2018-04-11 15:00:06 +0300

    added methods get, toString, equals to Vehicle

--- src/base/Vehicle.java
+++ src/base/Vehicle.java
@@ -6,6 +6,12 @@ public abstract class Vehicle {
 	private int seats;
 	private float speed;
 	
+	public String getModel() {return this.model;}
+	public double getTotalDistance() {return this.totalDistance;}
+	public int getSeats() {return this.seats;}
+	public float getSpeed() {return this.speed;}
+
+	
 	protected boolean moveDistance(float distance)
 	{
 		if (distance>0) {
@@ -21,5 +27,13 @@ public abstract class Vehicle {
 		this.totalDistance=0;
 		this.seats=seats;
 		this.speed=speed;
+	}
+	
+	public String toString() {
+		return this.getClass().getName()+": Model: "+this.model+", traveled:"+this.totalDistance+" Km, Max Speed of "+this.speed+" Mph, can carry max of "+this.seats+" people.";		
+	}
+	
+	public boolean equals(Vehicle other) {
+		return this.model.equals(other.getModel()) && this.totalDistance==other.getTotalDistance() && this.seats==other.getSeats() && this.speed==other.getSpeed();
 	}
 }
