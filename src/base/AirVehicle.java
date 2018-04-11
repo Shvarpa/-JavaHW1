68a444a9f84265d13a0b642827df07c62a8e6f0d
Pavel Shvarchov <shvarpa@gmail.com>
2018-04-11 15:28:48 +0300

    added get, toString, equals to AirVehicle

--- src/base/AirVehicle.java
+++ src/base/AirVehicle.java
@@ -6,6 +6,8 @@ public abstract class AirVehicle extends Vehicle{
 	private String vehicleUse;
 	static String[] possibleVehicleUse= {"Army","Civilian"};
 	
+	public String getVehicleUse() {return this.vehicleUse;}
+	
 	private static boolean checkVehicleUseInput(String vehicleUse) {
 		return Arrays.asList(AirVehicle.possibleVehicleUse).contains(vehicleUse);
 	}
@@ -21,5 +23,13 @@ public abstract class AirVehicle extends Vehicle{
 	protected AirVehicle(String model, int seats, float speed,String vehicleUse) {
 		super(model,seats,speed);
 		setVehicleUse(vehicleUse);
+	}
+	
+	public String toString() {
+		return super.toString()+" intended for "+this.vehicleUse+" use.";
+	}
+	
+	public boolean equals(AirVehicle other) {
+		return super.equals(other) && this.vehicleUse.equals(other.getVehicleUse());
 	}
 }
