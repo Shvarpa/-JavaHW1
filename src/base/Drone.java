b108d9721aae9d43a0455b8a7ef977e0a3362426
mordy <mordydd91@gmail.com>
2018-04-15 21:51:03 +0300

    fixing stuff

--- src/base/Drone.java
+++ src/base/Drone.java
@@ -1,35 +1,42 @@
 package base;
 
-public abstract class Drone extends AirVehicle implements NonMotorized{
-	
-	private String energyRating; //NonMotorized Interface
-	private String energySource;
-	
-	private boolean setEnergyRating(String energyRating) {
-		if(NonMotorized.checkEnergyRatingInput(energyRating)) {
-			this.energyRating=energyRating;
-			return true;
-		}
-		return false;
-	}
-	
-	private boolean setEnergySource(String energySource) {
-		this.energySource=energySource;
-		return true;
-	}
-	
-	public String getEnergyRating() {return energyRating;}
-	public String getEnergySource() {return energySource;}
+public abstract class Drone extends AirVehicle implements NonMotorized {
 
-	
-	protected Drone(String model, int seats, float speed,String vehicleUse,String energySource,String energyRating) {
-		super(model,seats,speed,vehicleUse);
-		setEnergySource(energySource);
-		setEnergyRating(energyRating);
-	}
-	
-	public String toString() {return super.toString()+" "+NonMotorized.toString(this);}
-	public boolean equals(Object other) {
-		return super.equals(other) && NonMotorized.equals(this, other);
-	}
+    private String energyRating; //NonMotorized Interface
+    private String energySource;
+
+    protected Drone(String model, int seats, float speed, String vehicleUse, String energySource, String energyRating) {
+        super(model, seats, speed, vehicleUse);
+        setEnergySource(energySource);
+        setEnergyRating(energyRating);
+    }
+
+    private boolean setEnergyRating(String energyRating) {
+        if (NonMotorized.checkEnergyRatingInput(energyRating)) {
+            this.energyRating = energyRating;
+            return true;
+        }
+        return false;
+    }
+
+    private boolean setEnergySource(String energySource) {
+        this.energySource = energySource;
+        return true;
+    }
+
+    public String getEnergyRating() {
+        return energyRating;
+    }
+
+    public String getEnergySource() {
+        return energySource;
+    }
+
+    public String toString() {
+        return super.toString() + " " + NonMotorized.toString(this);
+    }
+
+    public boolean equals(Object other) {
+        return super.equals(other) && NonMotorized.equals(this, other);
+    }
 }
