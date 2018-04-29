4db4f0bf5256559d72628be36b3303165e2923c3
Pavel Shvarchov <shvarpa@gmail.com>
2018-04-29 18:35:25 +0300

    gui

--- src/gui/Form.java
+++ src/gui/Form.java
@@ -7,6 +7,19 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import com.jgoodies.forms.layout.FormSpecs;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.SwingConstants;
 
 public class Form extends JDialog {
 
@@ -18,18 +31,54 @@ public class Form extends JDialog {
 	public Form() {
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		GridBagLayout gbl_contentPanel = new GridBagLayout();
+		gbl_contentPanel.columnWidths = new int[]{212, 212, 0};
+		gbl_contentPanel.rowHeights = new int[]{218, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPanel.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPanel.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPanel.setLayout(gbl_contentPanel);
+		{
+			JPanel panel = new JPanel();
+			GridBagConstraints gbc_panel = new GridBagConstraints();
+			gbc_panel.gridwidth = 2;
+			gbc_panel.insets = new Insets(0, 0, 5, 0);
+			gbc_panel.fill = GridBagConstraints.BOTH;
+			gbc_panel.gridx = 0;
+			gbc_panel.gridy = 0;
+			contentPanel.add(panel, gbc_panel);
+			panel.setLayout(new FlowLayout(FlowLayout.LEFT, 20, 5));
+			{
+				JLabel selectLabel = new JLabel("Select Type:");
+				selectLabel.setHorizontalAlignment(SwingConstants.CENTER);
+				panel.add(selectLabel);
+			}
+			{
+				JComboBox typesComboBox = new JComboBox();
+				typesComboBox.setModel(new DefaultComboBoxModel(new String[] {"Jeep", "Frigate", "SpyDrone", "PlayDrone", "AmphibiousVehicle", "Bike", "CruiseShip"}));
+				panel.add(typesComboBox);
+			}
+		}
+		{
+			JPanel panel = new JPanel();
+			GridBagConstraints gbc_panel = new GridBagConstraints();
+			gbc_panel.gridheight = 6;
+			gbc_panel.gridwidth = 2;
+			gbc_panel.fill = GridBagConstraints.BOTH;
+			gbc_panel.gridx = 0;
+			gbc_panel.gridy = 1;
+			contentPanel.add(panel, gbc_panel);
+		}
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
+				JButton addButton = new JButton("Add");
+				addButton.setActionCommand("OK");
+				buttonPane.add(addButton);
+				getRootPane().setDefaultButton(addButton);
 			}
 			{
 				JButton cancelButton = new JButton("Cancel");
