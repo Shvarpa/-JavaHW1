61082803deb5b178280796540266c107610e7005
Pavel Shvarchov <shvarpa@gmail.com>
2018-06-08 15:24:13 +0300

    improved StatusReporter to working condition, might be buggy

--- src/classes/StatusReporter.java
+++ src/classes/StatusReporter.java
@@ -1,12 +1,8 @@
 package classes;
 
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.util.ArrayList;
 import java.util.List;
-
-import com.sun.javafx.property.adapter.PropertyDescriptor.Listener;
 
 import gui.VehicleSelectButton;
 import interfaces.IVehicle;
@@ -27,39 +23,40 @@ public class StatusReporter implements IVehicle {
 
 	private IVehicle vehicle;
 	private VehicleStatus status;
-    private final PropertyChangeSupport eventDispatcher = new PropertyChangeSupport(this);
-	public void notifyDrawings() {
-		eventDispatcher.firePropertyChange("statusChanged", null, this);
-	}
-	public StatusReporter(IVehicle v,VehicleStatus status) {
-		if(v instanceof StatusReporter) {
-			((StatusReporter)v).notifyDrawings();
-		}
-		this.vehicle = v;
-		this.status = status;
-	}
+	private List<VehicleSelectButton> drawings;
+	private PropertyChangeSupport eventHandler;
 	
-	List<VehicleSelectButton> drawings = new ArrayList<VehicleSelectButton>();
-	private void changeOwner(VehicleSelectButton drawing, IVehicle newOwner) {
-		drawings.remove(drawing);
-		if (newOwner instanceof StatusReporter) {
-			StatusReporter owner = ((StatusReporter)newOwner);
-			owner.drawings.add(drawing);
-			updateDrawing(drawing, owner);
+	public StatusReporter(IVehicle vehicle, VehicleStatus status) {
+		if (vehicle instanceof StatusReporter) {
+			StatusReporter other = (StatusReporter)vehicle;
+			other.status = status;
+			clone(other);
+			updateDrawings();
+		}
+		else {
+			this.vehicle = vehicle;
+			this.status = status;
+			this.drawings = new ArrayList<VehicleSelectButton>();
+			this.eventHandler = new PropertyChangeSupport(this);
 		}
 	}
 	
-	private void updateDrawing(VehicleSelectButton drawing, IVehicle owner) {
-		drawing.setVehicle(owner);
-		drawing.setToolTipText(owner.toString());
-		PropertyChangeListener listener = new PropertyChangeListener() {
-			@Override
-			public void propertyChange(PropertyChangeEvent event) {
-				drawing.removePropertyChangeListener(this);
-				changeOwner(drawing, (IVehicle)event.getNewValue());
-			}
-		};
-		drawing.addPropertyChangeListener("statusChanged",listener);
+	
+	///intended shallow clone of other StatusReporter
+	private void clone(StatusReporter other) {
+		this.drawings = other.drawings;
+		this.vehicle = other.vehicle;
+		this.status = other.status;
+		this.eventHandler = other.eventHandler;
+	}
+	
+	private void updateDrawings() {
+		eventHandler.firePropertyChange("statusUpdate", null, null);
+	}
+	
+	private void updateDrawing(VehicleSelectButton drawing) {
+		drawing.setVehicle(this);
+		drawing.setToolTipText(this.toString());
 		drawing.revalidate();
 		drawing.repaint();
 	}
@@ -67,8 +64,11 @@ public class StatusReporter implements IVehicle {
 	@Override
 	public VehicleSelectButton draw() {
 		VehicleSelectButton drawing = vehicle.draw();
-		updateDrawing(drawing, this);
+		updateDrawing(drawing);
 		drawings.add(drawing);
+		eventHandler.addPropertyChangeListener("statusUpdate",(event)->{
+			updateDrawing(drawing);
+		});
 		return drawing;
 	}
 	
@@ -103,15 +103,9 @@ public class StatusReporter implements IVehicle {
 		return vehicle.moveDistance(distance);
 	}
 	
-	private String originalToString() {
-		if(vehicle instanceof StatusReporter) {
-			return ((StatusReporter) vehicle).originalToString();
-		}
-		return vehicle.toString();
-	}
 	@Override
 	public String toString() {
-		return originalToString() + status;
+		return vehicle.toString() + status;
 	}
 	
 	@Override
