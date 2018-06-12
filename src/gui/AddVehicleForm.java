b854feb5dc4ed2161e0face1de15d594a3f78b2e
Pavel Shvarchov <shvarpa@gmail.com>
2018-06-12 22:35:30 +0300

    overhauled AddVehicleForm for better performance

--- src/gui/AddVehicleForm.java
+++ src/gui/AddVehicleForm.java
@@ -7,62 +7,55 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
-
 import javax.swing.JComboBox;
-import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
-
 import classes.AmphibiousVehicle;
 import classes.Bike;
 import classes.CruiseShip;
 import classes.ElectricBike;
 import classes.Frigate;
 import classes.HybridPlane;
-import classes.Input;
 import classes.Jeep;
 import classes.PlayDrone;
 import classes.SpyDrone;
+import gui.FormParameter;
+import gui.FormParameter.Parameter;
 import interfaces.Commercial;
 import interfaces.IAirVehicle;
 import interfaces.ILandVehicle;
 import interfaces.IVehicle;
 import interfaces.NonMotorized;
-import javafx.util.Pair;
 
 public class AddVehicleForm extends JPanel {
 	private static final long serialVersionUID = 1L;
 	///the addVehicleForm contains all the possible vehicle inputs and their GUI representation, and selects to show the required fields according to a vehicle type specified at the vehicle type comboBox.
 	///the form attempts to create the require vehicle type with the vehicle inputs given, and if unsuccessful throws a numberFormateException detailing the input failure.
+		
+	private FormParameter<String> model = new FormParameter<String>(Parameter.model,String.class);
+	private FormParameter<String> energySource = new FormParameter<String>(Parameter.energySource,String.class);
+	private FormParameter<String> vehicleUse = new FormParameter<String>(Parameter.vehicleUse,String.class);
+	private FormParameter<String> roadType = new FormParameter<String>(Parameter.roadType,String.class);
+	private FormParameter<String> licence = new FormParameter<String>(Parameter.licence,String.class);
+	private FormParameter<String> energyRating = new FormParameter<String>(Parameter.energyRating,String.class);
+	private FormParameter<String> flag = new FormParameter<String>(Parameter.flag,String.class);
+
+
+	private FormParameter<Integer> seats = new FormParameter<Integer>(Parameter.seats,Integer.class);
+	private FormParameter<Integer> wheels = new FormParameter<Integer>(Parameter.wheels,Integer.class);
+
+	private FormParameter<Float> speed = new FormParameter<Float>(Parameter.speed,Float.class);
 	
-	private Pair<JLabel, JTextField> model = new Pair<JLabel, JTextField>(new JLabel("model"), new JTextField());
-	private Pair<JLabel, JTextField> seats = new Pair<JLabel, JTextField>(new JLabel("seats"), new JTextField());
-	private Pair<JLabel, JTextField> speed = new Pair<JLabel, JTextField>(new JLabel("speed"), new JTextField());
-	private Pair<JLabel, JTextField> wheels = new Pair<JLabel, JTextField>(new JLabel("wheels"), new JTextField());
+	private FormParameter<Double> avgMotorLifespan = new FormParameter<Double>(Parameter.avgMotorLifespan,Double.class);
+	private FormParameter<Double> avgFuelConsumption = new FormParameter<Double>(Parameter.avgFuelConsumption,Double.class);
 
-	private Pair<JLabel, JTextField> avgMotorLifespan = new Pair<JLabel, JTextField>(new JLabel("avg motor lifespan"), new JTextField());
-	private Pair<JLabel, JTextField> avgFuelConsumption = new Pair<JLabel, JTextField>(new JLabel("avg fuel consumption"), new JTextField());
-	private Pair<JLabel, JTextField> energySource = new Pair<JLabel, JTextField>(new JLabel("energy source"), new JTextField());
+	private FormParameter<Boolean> withWindDiraction = new FormParameter<Boolean>(Parameter.withWindDiraction,Boolean.class);
 
-	private Pair<JLabel, JRadioButton> withWindDiraction = new Pair<JLabel, JRadioButton>(new JLabel("with wind diraction?"),
-			new JRadioButton());
-
-	private Pair<JLabel, JComboBox<String>> vehicleUse = new Pair<JLabel, JComboBox<String>>(new JLabel("vehicle use"),
-			new JComboBox<String>(IAirVehicle.possibleVehicleUse.toStringArray()));
-	private Pair<JLabel, JComboBox<String>> roadType = new Pair<JLabel, JComboBox<String>>(new JLabel("road type"),
-			new JComboBox<String>(ILandVehicle.possibleRoadType.toStringArray()));
-	private Pair<JLabel, JComboBox<String>> licence = new Pair<JLabel, JComboBox<String>>(new JLabel("licence"),
-			new JComboBox<String>(Commercial.possibleLicences.toStringArray()));
-	private Pair<JLabel, JComboBox<String>> energyRating = new Pair<JLabel, JComboBox<String>>(new JLabel("energy rating"),
-			new JComboBox<String>(NonMotorized.possibleEnergyRating.toStringArray()));
-	
-	private Pair<JLabel, JComboBox<ImageText>> flag = new Pair<JLabel, JComboBox<ImageText>>(new JLabel("flag"),
-			Utilities.createFlagsComboBox(new Dimension(70, 50)));
-
-	private List<Pair<JLabel,?>> shown = new ArrayList<Pair<JLabel,?>>();
+	private List<FormParameter<?>> shown = new ArrayList<FormParameter<?>>();
 	private String vehicleType;
 	private int currentRow = 0;
 	
@@ -70,30 +63,72 @@ public class AddVehicleForm extends JPanel {
 		setLayout(new GridBagLayout());
 		show(vehicleType);
 	}
-	
-	private void clear(Pair<JLabel,?> p) {
+	private void remove(FormParameter<?> p) {
 		this.remove(p.getKey());
-		Object v = p.getValue();
-		if (v instanceof JComponent) this.remove((JComponent) v);
+		this.remove(p.getVal());
 	}
-	
 	private void clear() {
-		for (Pair<JLabel,?> p : shown) {
-			Object val = p.getValue();
-			if (val instanceof JComponent) ((JComponent)val).setEnabled(true);
-			if(val instanceof JTextField) {((JTextField) val).setText("");}
-			clear(p);
+		for (FormParameter<?> p : shown) {
+			p.restoreDefault();
+			this.remove(p);
 		}
 		this.currentRow = 0;
 		shown.clear();
 	}
-	
-	private void show(Pair<JLabel, ?> p) {
-		Object v = p.getValue();
-		if (!(v instanceof JComponent)) return;
-		JComponent key = p.getKey();
-		JComponent val = (JComponent)v;
+	private void initiate(FormParameter<?> p) {
+		if(!p.initialized()) {
+			switch(p.getParameter()){
+			case model: 
+				this.model.initialize(new JLabel("model"), new JTextField());
+				break;
+			case seats: 
+				this.seats.initialize(new JLabel("seats"), new JTextField());
+				break;
+			case speed:
+				this.speed.initialize(new JLabel("speed"), new JTextField());
+				break;
+			case wheels: 
+				this.wheels.initialize(new JLabel("wheels"), new JTextField());
+				break;
+			case avgMotorLifespan: 
+				this.avgMotorLifespan.initialize(new JLabel("avg motor lifespan"), new JTextField());
+				break;
+			case avgFuelConsumption: 
+				this.avgFuelConsumption.initialize(new JLabel("avg fuel consumption"), new JTextField());
+				break;
+			case energySource: 
+				this.energySource.initialize(new JLabel("energy source"), new JTextField());
+				break;
+			case withWindDiraction:
+				this.withWindDiraction.initialize(new JLabel("with wind diraction?"),new JRadioButton());
+				break;
+			case vehicleUse:
+				this.vehicleUse.initialize(new JLabel("vehicle use"),new JComboBox<String>(IAirVehicle.possibleVehicleUse.toStringArray()));
+				break;
+			case roadType:
+				this.roadType.initialize(new JLabel("road type"),new JComboBox<String>(ILandVehicle.possibleRoadType.toStringArray()));
+				break;
+			case licence:
+				this.licence.initialize(new JLabel("licence"),new JComboBox<String>(Commercial.possibleLicences.toStringArray()));
+				break;
+			case energyRating: 
+				this.energyRating.initialize(new JLabel("energy rating"),new JComboBox<String>(NonMotorized.possibleEnergyRating.toStringArray()));
+				break;
+			case flag: 
+				this.flag.initialize(new JLabel("flag"),Utilities.createFlagsComboBox(new Dimension(70, 50)));
+				break;
+			default:
+				break;
+			}
+		}
 		
+	}
+	private void initiate(Collection<FormParameter<?>>params) {
+		for(FormParameter<?> p : params)
+			initiate(p);
+	}
+
+	private void show(FormParameter<?> param) {
 		GridBagConstraints gbc;
 		gbc = new GridBagConstraints();
 		gbc.gridy=this.currentRow;
@@ -103,7 +138,7 @@ public class AddVehicleForm extends JPanel {
 		gbc.weightx=0;
 		gbc.weighty=0;
 		gbc.anchor=GridBagConstraints.EAST;
-		this.add(key,gbc);
+		this.add(param.getKey(),gbc);
 		
 		gbc = new GridBagConstraints();
 		gbc.gridy=this.currentRow;
@@ -114,104 +149,85 @@ public class AddVehicleForm extends JPanel {
 		gbc.weighty=0;
 		gbc.gridwidth=3;
 		gbc.fill=GridBagConstraints.HORIZONTAL;
-		this.add(val,gbc);
+		this.add(param.getVal(),gbc);
 		
-		shown.add(p);
+		shown.add(param);
 		this.currentRow++;
 	}
 
-	private void show(List<Pair<JLabel, ?>> list) {
-		for(Pair<JLabel, ?> p:list) {
-			show(p);
+	private void show(List<FormParameter<?>> list) {
+		initiate(list);
+		for(FormParameter<?> fP:list) {
+			show(fP);
 		}
 	}
-	
 
 	public void show(String type) {
 		this.vehicleType = type;
 		clear();
 		switch (this.vehicleType) {
 			case "Jeep":
-				seats.getValue().setText(Integer.toString(Jeep.defaultSeats));
-				seats.getValue().setEnabled(false);
-				wheels.getValue().setText(Integer.toString(Jeep.defaultWheels));
-				wheels.getValue().setEnabled(false);
-				roadType.getValue().setSelectedItem(Jeep.defaultRoadType);
-				roadType.getValue().setEnabled(false);
-				licence.getValue().setSelectedItem(Jeep.defaultLicence);
-				licence.getValue().setEnabled(false);
+				initiate(Arrays.asList(seats,wheels,roadType,licence));
+				seats.setDefault(Integer.toString(Jeep.defaultSeats));
+				wheels.setDefault(Integer.toString(Jeep.defaultWheels));
+				roadType.setDefault(Jeep.defaultRoadType);
+				licence.setDefault(Jeep.defaultLicence);
 				show(Arrays.asList(model,seats,speed,wheels,roadType,avgFuelConsumption,avgMotorLifespan,licence));
 				break;
 			case "Frigate":
-				avgFuelConsumption.getValue().setText(Double.toString(Frigate.defaultAvgFuelConsumption));
-				avgFuelConsumption.getValue().setEnabled(false);
-				avgMotorLifespan.getValue().setText(Double.toString(Frigate.defaultAvgMotorLifespan));
-				avgMotorLifespan.getValue().setEnabled(false);
-				flag.getValue().setSelectedItem(Frigate.defaultFlag);
-				flag.getValue().setEnabled(false);
+				initiate(Arrays.asList(avgFuelConsumption,avgMotorLifespan,flag));
+				avgFuelConsumption.setDefault(Double.toString(Frigate.defaultAvgFuelConsumption));
+				avgMotorLifespan.setDefault(Double.toString(Frigate.defaultAvgMotorLifespan));
+				flag.setDefault(Frigate.defaultFlag);
 				show(Arrays.asList(model,seats,speed,withWindDiraction,avgFuelConsumption,avgMotorLifespan,flag));	
 				break;
 			case "SpyDrone":
-				model.getValue().setText(SpyDrone.defaultModel);
-				model.getValue().setEnabled(false);
-				seats.getValue().setText(Integer.toString(SpyDrone.defaultSeats));
-				seats.getValue().setEnabled(false);
-				speed.getValue().setText(Float.toString(SpyDrone.defaultSpeed));
-				speed.getValue().setEnabled(false);
-				vehicleUse.getValue().setSelectedItem(SpyDrone.defaultVehicleUse);
-				vehicleUse.getValue().setEnabled(false);
-				energyRating.getValue().setSelectedItem(SpyDrone.defaultEnergyRating);
-				energyRating.getValue().setEnabled(false);
+				initiate(Arrays.asList(model,seats,speed,vehicleUse,energyRating));
+				model.setDefault(SpyDrone.defaultModel);
+				seats.setDefault(Integer.toString(SpyDrone.defaultSeats));
+				speed.setDefault(Float.toString(SpyDrone.defaultSpeed));
+				vehicleUse.setDefault(SpyDrone.defaultVehicleUse);
+				energyRating.setDefault(SpyDrone.defaultEnergyRating);
 				show(Arrays.asList(model,seats,speed,vehicleUse,energySource,energyRating));	
 				break;
 			case "PlayDrone":
-				model.getValue().setText(PlayDrone.defaultModel);
-				model.getValue().setEnabled(false);
-				seats.getValue().setText(Integer.toString(PlayDrone.defaultSeats));
-				seats.getValue().setEnabled(false);
-				speed.getValue().setText(Float.toString(PlayDrone.defaultSpeed));
-				speed.getValue().setEnabled(false);
-				vehicleUse.getValue().setSelectedItem(PlayDrone.defaultVehicleUse);
-				vehicleUse.getValue().setEnabled(false);
-				energySource.getValue().setText(PlayDrone.defaultEnergySource);
-				energySource.getValue().setEnabled(false);
-				energyRating.getValue().setSelectedItem(PlayDrone.defaultEnergyRating);
-				energyRating.getValue().setEnabled(false);
+				initiate(Arrays.asList(model,seats,speed,vehicleUse,energySource,energyRating));
+				model.setDefault(PlayDrone.defaultModel);
+				seats.setDefault(Integer.toString(PlayDrone.defaultSeats));
+				speed.setDefault(Float.toString(PlayDrone.defaultSpeed));
+				vehicleUse.setDefault(PlayDrone.defaultVehicleUse);
+				energySource.setDefault(PlayDrone.defaultEnergySource);
+				energyRating.setDefault(PlayDrone.defaultEnergyRating);
 				show(Arrays.asList(model,seats,speed,vehicleUse,energySource,energyRating));	
 				break;
 			case "AmphibiousVehicle":
-				roadType.getValue().setSelectedItem(AmphibiousVehicle.defaultRoadType);
-				roadType.getValue().setEnabled(false);
+				initiate(Arrays.asList(roadType));
+				roadType.setDefault(AmphibiousVehicle.defaultRoadType);
 				show(Arrays.asList(model,seats,speed,wheels,roadType,avgFuelConsumption,avgMotorLifespan,withWindDiraction,flag));
 				break;
 			case "Bike":
-				wheels.getValue().setText(Integer.toString(Bike.defaultWheels));
-				wheels.getValue().setEnabled(false);
-				energyRating.getValue().setSelectedItem(Bike.defaultEnergyRating);
-				energyRating.getValue().setEnabled(false);
-				energySource.getValue().setText(Bike.defaultEnergySource);
-				energySource.getValue().setEnabled(false);
+				initiate(Arrays.asList(wheels,energySource,energyRating));
+				wheels.setDefault(Integer.toString(Bike.defaultWheels));
+				energyRating.setDefault(Bike.defaultEnergyRating);
+				energySource.setDefault(Bike.defaultEnergySource);
 				show(Arrays.asList(model,seats,speed,wheels,roadType,energySource,energyRating));
 				break;
 			case "CruiseShip":
-				licence.getValue().setSelectedItem(CruiseShip.defaultLicence);
-				licence.getValue().setEnabled(false);
-				withWindDiraction.getValue().setSelected(CruiseShip.defaultWithWindDiraction);
-				withWindDiraction.getValue().setEnabled(false);
+				initiate(Arrays.asList(withWindDiraction,licence));
+				licence.setDefault(CruiseShip.defaultLicence);
+				withWindDiraction.setDefault(CruiseShip.defaultWithWindDiraction);
 				show(Arrays.asList(model,seats,speed,withWindDiraction,avgFuelConsumption,avgMotorLifespan,licence,flag));
 				break;
 			case "HybridPlane":
-				vehicleUse.getValue().setSelectedItem(HybridPlane.defaultVehicleUse);
-				vehicleUse.getValue().setEnabled(false);
-				roadType.getValue().setSelectedItem(HybridPlane.defaultRoadType);
-				roadType.getValue().setEnabled(false);
+				initiate(Arrays.asList(vehicleUse,roadType));
+				vehicleUse.setDefault(HybridPlane.defaultVehicleUse);
+				roadType.setDefault(HybridPlane.defaultRoadType);
 				show(Arrays.asList(model,seats,speed,wheels,withWindDiraction,avgFuelConsumption,avgMotorLifespan,vehicleUse,roadType,flag));
 				break;
 			case "ElectricBike":
-				wheels.getValue().setText(Integer.toString(ElectricBike.defaultWheels));
-				wheels.getValue().setEnabled(false);
-				avgFuelConsumption.getValue().setText(Double.toString(ElectricBike.defaultAvgFuelConsumption));
-				avgFuelConsumption.getValue().setEnabled(false);
+				initiate(Arrays.asList(wheels,avgMotorLifespan));
+				wheels.setDefault(Integer.toString(ElectricBike.defaultWheels));
+				avgFuelConsumption.setDefault(Double.toString(ElectricBike.defaultAvgFuelConsumption));
 				show(Arrays.asList(model,seats,speed,wheels,avgFuelConsumption,avgMotorLifespan,roadType));
 				break;
 		}
@@ -221,64 +237,24 @@ public class AddVehicleForm extends JPanel {
 	public IVehicle createVehicle() throws NumberFormatException{
 		switch (this.vehicleType) {
 		case "Jeep":
-			return new Jeep(getString(model), getFloat(speed), getDouble(avgFuelConsumption), getDouble(avgMotorLifespan));
+			return new Jeep(model.get(), speed.get(), avgFuelConsumption.get(), avgMotorLifespan.get());
 		case "Frigate":
-			return new Frigate(getString(model), getInt(seats), getFloat(speed), getBoolean(withWindDiraction));
+			return new Frigate(model.get(), seats.get(), speed.get(), withWindDiraction.get());
 		case "SpyDrone":
-			return new SpyDrone(getString(energySource));
+			return new SpyDrone(energySource.get());
 		case "PlayDrone":
 			return new PlayDrone();
 		case "AmphibiousVehicle":
-			return new AmphibiousVehicle(getString(model), getInt(seats), getFloat(speed), getInt(wheels), getBoolean(withWindDiraction), getString(flag), getDouble(avgFuelConsumption), getDouble(avgMotorLifespan));
+			return new AmphibiousVehicle(model.get(), seats.get(), speed.get(), wheels.get(), withWindDiraction.get(), flag.get(), avgFuelConsumption.get(), avgMotorLifespan.get());
 		case "Bike":
-			return new Bike(getString(model), getInt(seats), getFloat(speed), getString(roadType));
+			return new Bike(model.get(), seats.get(), speed.get(), roadType.get());
 		case "CruiseShip":
-			return new CruiseShip(getString(model), getInt(seats), getFloat(speed), getString(flag), getDouble(avgFuelConsumption), getDouble(avgMotorLifespan));
+			return new CruiseShip(model.get(), seats.get(), speed.get(), flag.get(), avgFuelConsumption.get(), avgMotorLifespan.get());
 		case "HybridPlane":
-			return new HybridPlane(getString(model), getInt(seats), getFloat(speed), getInt(wheels), getBoolean(withWindDiraction), getString(flag), getDouble(avgFuelConsumption), getDouble(avgMotorLifespan));
+			return new HybridPlane(model.get(), seats.get(), speed.get(), wheels.get(), withWindDiraction.get(), flag.get(), avgFuelConsumption.get(), avgMotorLifespan.get());
 		case "ElectricBike":
-			return new ElectricBike(getString(model), getInt(seats), getFloat(speed), getString(roadType), getDouble(avgMotorLifespan));
-	}
+			return new ElectricBike(model.get(), seats.get(), speed.get(), roadType.get(), avgMotorLifespan.get());
+		}
 		return null;
-}
-	
-	private Integer getInt(Pair<JLabel, ?> p) throws NumberFormatException {
-		Object val = p.getValue();
-		if(!(val instanceof JComponent)) return null;
-		String target = p.getKey().getText().toString();
-		if (val instanceof JTextField) return (int) Input.parsePositive(target, Input.parseInt(target, ((JTextField) val).getText().toString()));
-		return null;
-	}
-	
-	private Float getFloat(Pair<JLabel, ?> p) throws NumberFormatException {
-		Object val = p.getValue();
-		if(!(val instanceof JComponent)) return null;
-		String target = p.getKey().getText().toString();
-		if (val instanceof JTextField) return (float) Input.parsePositive(target, Input.parseFloat(target, ((JTextField) val).getText().toString()));
-		return null;
-	}
-	
-	private Double getDouble(Pair<JLabel, ?> p) throws NumberFormatException {
-		Object val = p.getValue();
-		if(!(val instanceof JComponent)) return null;
-		String target = p.getKey().getText().toString();
-		if (val instanceof JTextField) return (double) Input.parsePositive(target, Input.parseDouble(target, ((JTextField) val).getText()));
-		else return null;
-	}
-	
-	private String getString(Pair<JLabel, ?> p) {
-		Object val = p.getValue();
-		if(!(val instanceof JComponent)) return null;
-		if (val instanceof JTextField)	return ((JTextField)val).getText().toString();
-		else if (val instanceof JComboBox<?>) return ((JComboBox<?>)val).getSelectedItem().toString();
-		else return null;
-	}
-	
-	private Boolean getBoolean(Pair<JLabel, ?> p) {
-		Object val = p.getValue();
-		if(!(val instanceof JComponent)) return null;
-		if (val instanceof JRadioButton) return ((JRadioButton)val).isSelected();
-		else return null;
-	}
-	
+	}	
 }
---
+++ src/gui/FormParameter.java
@@ -0,0 +1,115 @@
+package gui;
+
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import classes.Input;
+
+class FormParameter<T>{
+	enum Parameter{model, seats, speed, wheels, avgMotorLifespan, avgFuelConsumption, energySource, withWindDiraction, vehicleUse, roadType, licence, energyRating, flag;}
+	private Parameter param;
+	private JLabel key;
+	private JComponent val;
+	private Class<T> returnType;
+	private boolean initialized;
+	FormParameter(Parameter param,Class<T> c, JLabel key,JComponent val) {
+		this.param = param;
+		this.returnType = c;
+		initialize(key,val);
+	}
+	FormParameter(Parameter param,Class<T> c) {
+		this.param = param;
+		this.returnType = c;
+	}
+	void initialize(JLabel key,JComponent val){
+		if (!initialized) {
+			initialized = true;
+			this.key = key;
+			this.val = val;
+		}
+	}
+	
+	JLabel getKey(){
+		return this.key;
+	}
+	JComponent getVal() {
+		return this.val;
+	}
+	
+	Parameter getParameter() {
+		return param;
+	}
+	private String getText() {
+		if (val instanceof JTextField)	return ((JTextField)val).getText().toString();
+		else if (val instanceof JComboBox<?>) return ((JComboBox<?>)val).getSelectedItem().toString();
+		else return null;
+	}
+	private String getTarget() {
+		return key.getText().toString();
+	}
+		
+	private Integer getInt() throws NumberFormatException {
+		String target = getTarget();
+		if (val instanceof JTextField) return (int) Input.parsePositive(target, Input.parseInt(target, ((JTextField) val).getText().toString()));
+		return null;
+	}
+	
+	private Float getFloat() throws NumberFormatException {
+		String target = getTarget();
+		if (val instanceof JTextField) return (float) Input.parsePositive(target, Input.parseFloat(target, ((JTextField) val).getText().toString()));
+		return null;
+	}
+	
+	private Double getDouble() throws NumberFormatException {
+		String target = getTarget();
+		if (val instanceof JTextField) return (double) Input.parsePositive(target, Input.parseDouble(target, ((JTextField) val).getText()));
+		else return null;
+	}
+		
+	private Boolean getBoolean() {
+		if (val instanceof JRadioButton) return ((JRadioButton)val).isSelected();
+		String text = getText();
+		if (text!=null) return Input.parseBoolean(getTarget(), text);
+		else return null;
+	}
+	private void setText(Object text) {
+		if (val instanceof JTextField) ((JTextField) val).setText(text.toString());
+	}
+	private void setSelectedItem(Object item) {
+		if(val instanceof JComboBox<?>) ((JComboBox<?>) val).setSelectedItem(item);
+	}
+	private void setEnabled(boolean b) { 
+		val.setEnabled(b);
+	}
+	void setDefault(Object text) {
+		setText(text);
+		setSelectedItem(text);
+		setEnabled(false);
+	}
+	void restoreDefault() {
+		setText("");
+		setEnabled(false);
+	}
+	@SuppressWarnings("unchecked")
+	T get() {
+		String ret = returnType.getSimpleName();
+		if(ret.equals(String.class.getSimpleName()))
+			return (T)getText();
+		else if(ret.equals(Integer.class.getSimpleName()))
+			return (T)getInt();
+		else if (ret.equals(Float.class.getSimpleName()))
+			return (T)getFloat();
+		else if (ret.equals(Double.class.getSimpleName()))
+			return (T)getDouble();
+		else if (ret.equals(Boolean.class.getSimpleName()))
+			return (T)getBoolean();
+		return null;
+	}
+	
+	boolean initialized() {
+		return initialized;
+	}
+	
+}
